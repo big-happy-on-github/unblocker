9babff439c7b6240d09e7d9e66f3938b6f67befb
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-11-08 16:18:28 -0500

    Update fetch-site.js

--- netlify/functions/fetch-site.js
+++ netlify/functions/fetch-site.js
@@ -1,29 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event) {
-    const url = event.queryStringParameters.url;
-    if (!url) {
-        return {
-            statusCode: 400,
-            body: 'URL is required',
-        };
-    }
     
-    try {
-        const response = await fetch(url);
-        const data = await response.text();
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'text/html',
-            },
-            body: data,
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: `Fetch error: ${error.message}`,
-        };
-    }
 };
